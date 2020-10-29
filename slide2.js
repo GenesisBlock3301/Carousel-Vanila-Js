053cb85e79ab10499e0aa0e4dd750599caba95ee
NurAminSifatTanha <nur15-1463@diu.edu.bd>
2020-10-29 22:21:51 +0600

    carousel with pure js

---
+++ slide2.js
@@ -0,0 +1,44 @@
+const slides = document.querySelector('.slider-items').children;
+const nextSlide = document.querySelector(".right-slide");
+const prevSlide = document.querySelector(".left-slide");
+const totalSlides = slides.length;
+let index = 0;
+
+nextSlide.onclick=function () {
+     next("next");
+};
+prevSlide.onclick=function () {
+     next("prev");
+};
+
+function next(direction){
+
+   if(direction=="next"){
+      index++;
+       if(index==totalSlides){
+        index=0;
+       }
+   }
+   else{
+           if(index==0){
+            index=totalSlides-1;
+           }
+           else{
+            index--;
+           }
+    }
+
+  for(let i=0;i < slides.length;i++){
+          slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+
+
+}
+
+
+
+
+
+
+
